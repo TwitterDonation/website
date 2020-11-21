679b3ba208ef5c938c333c8b833cbd1eaf478792
Omar Aflak <aflakomar@gmail.com>
2020-11-21 22:55:43 +0100

    amount input

--- public/static/index.js
+++ public/static/index.js
@@ -1,9 +1,9 @@
-const generateDepositLink = async twitterId => {
+const generateDepositLink = async (twitterId, currency, amount) => {
     return new Promise((resolve, reject) => {
         const returnUrl = 'https://twittercash-68b84.web.app'
         const cancelUrl = 'https://twittercash-68b84.web.app'
         const corsAnywhere = 'https://cors-anywhere.herokuapp.com/'
-        const url = `${corsAnywhere}https://us-central1-twittercash-68b84.cloudfunctions.net/webhook/paypal?currency=EUR&amount=1&return_url=${returnUrl}&cancel_url=${cancelUrl}&twitter_id=${twitterId}`
+        const url = `${corsAnywhere}https://us-central1-twittercash-68b84.cloudfunctions.net/webhook/paypal?currency=${currency}&amount=${amount}&return_url=${returnUrl}&cancel_url=${cancelUrl}&twitter_id=${twitterId}`
         $.get(url, (data, status) => {
             if (status == 'success') {
                 resolve(data.link)
@@ -18,14 +18,15 @@ window.onload = () => {
     const auth = firebase.auth()
     const db = firebase.firestore()
 
-    const btn = document.getElementById('login')
-    btn.onclick = () => {
+    const twitterLogin = document.getElementById('login')
+    twitterLogin.onclick = () => {
         const provider = new firebase.auth.TwitterAuthProvider()
         firebase.auth().signInWithPopup(provider)
             .catch(error => {
                 console.log(error)
             })
     }
+
 
     auth.onAuthStateChanged(user => {
         if (user) {
@@ -35,17 +36,19 @@ window.onload = () => {
             const depositDiv = document.getElementById('deposit-div')
             const text = document.getElementById('hello')
 
+            twitterLogin.style.visibility = 'hidden'
             text.innerHTML = `Hello ${displayName}!`
+            depositDiv.style.visibility = 'visible'
 
-            generateDepositLink(twitterId).then(url => {
-                deposit.onclick = () => {
+            deposit.onclick = () => {
+                const amount = parseInt(document.getElementById('amount').value)
+                const currency = document.getElementById('currency').selectedOptions[0].value
+                generateDepositLink(twitterId, currency, amount).then(url => {
                     window.open(url, '_blank')
-                }
-                depositDiv.removeAttribute('hidden')
-            }).catch(e => {
-                text.innerHTML = 'Something happened... Try again later.'
-                depositDiv.removeAttribute('hidden')
-            })
+                }).catch(e => {
+                    text.innerHTML = 'Something happened... Try again later.'
+                })
+            }
         }
     })
 }
